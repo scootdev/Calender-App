0b373ee4cc75b6490ed5a4a205e95a988392bfa0
Scott Harris <scottsharris@yahoo.com>
2020-04-30 10:08:31 -0700

    added function to change color of timeblocks based on the current time

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    var currentHour;
+
     // Initiation function
     function init() {
         // Set the current day & update the timer every second
@@ -6,9 +8,22 @@ $(document).ready(function () {
             $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
         }, 1000);
 
+        // set current hour
+        currentHour = "#hour-" + moment().hour();
+        $(currentHour).toggleClass("future").toggleClass("present");
+
+        // set past hours
+           if (moment().hour() > 9) {
+                for (var i = 9; i < moment().hour(); i++) {
+                var pastHour = "#hour-" + i;
+                $(pastHour).toggleClass("future").toggleClass("past");
+                
+            }
+        }       
     }
 
 
 
     init();
+    console.log(moment().hour());
 })
